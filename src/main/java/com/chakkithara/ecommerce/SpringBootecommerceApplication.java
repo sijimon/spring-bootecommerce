0433dd2fe50bf759dcea402131d1d018eefcabd1
sijimon <sijimonr@gmail.com>
2023-11-23 16:18:52 -0600

    Initial Commit

---
+++ src/main/java/com/chakkithara/ecommerce/SpringBootecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.chakkithara.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chakkithara/ecommerce/SpringBootecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chakkithara.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
